8c7791283bb7b62e398bd7e584fe1f012bcaf876
wadaudzai <wadaudzai@gmail.com>
2022-12-25 22:13:31 -0500

    Create react app

---
+++ src/walid.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Walid () {
+    return (
+        <div>
+            <h1>Walid</h1>
+            <p> walid is a good guy</p>
+        </div>
+    )
+}
+export default Walid;
