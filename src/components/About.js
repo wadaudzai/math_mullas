5a869c893c6c912b9b7976ec5f5a25f1ad883108
wadaudzai <wadaudzai@gmail.com>
2023-01-06 23:53:18 -0500

    Create about.js

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Calculator</h1>
+      <p>This is the calculator app.</p>
+    </div>
+  );
+}
+
+export default About;
